801a6cfafe5ec54294159b96898495ef1cadafbd
takejnguyenvy2710 <75810004+takejnguyenvy2710@users.noreply.github.com>
2022-07-10 20:48:28 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.nav').addClass("sticky");
+        }else{
+            $('.nav').removeClass("sticky");
+        }
+    })
+});
+
+   
